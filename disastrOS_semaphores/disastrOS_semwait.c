c7db4c609408f50260a8e78952ce90998c089bc2
pivarim <you@example.com>
2019-10-02 14:10:29 +0200

    function internal_semwait made,function definition in disastrOS.h and function call in disastros.c added

--- disastrOS_semaphores/disastrOS_semwait.c
+++ disastrOS_semaphores/disastrOS_semwait.c
@@ -7,6 +7,26 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semWait(){
-  int semaphore = running->syscall_args[0];
-  SemDescriptors semaphoreDe=
+  int sem_fd = running->syscall_args[0];
+  SemDescriptors sem_ds=SemDescriptorsListByFd(running->sem_descriptors,sem_fd);
+
+  Semaphore sem= sem_ds->Semaphore;
+
+  SemDescriptorPtr sem_ptr= sem_ds->ptr;
+
+  if(sem->count<0){
+
+  	List_insert(sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem*)sem_ds->ptr);
+  	
+  	running->status = Waiting;
+
+  	List_insert(waiting_list, waiting_list.last, (ListItem*) running);
+
+  	PCB* pcb = List_detach(&ready_list, (ListItem*)ready_list.first);
+
+  	running = (PCB*)pcb;
+
+  }
+
+  return;
 }
