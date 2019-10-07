fe02efd906b76b624a1c3cc8e98ba58d4342ad99
pivarim <you@example.com>
2019-10-07 11:16:27 +0200

    comments is added to semwait and sempost

--- disastrOS_semaphores/disastrOS_sempost.c
+++ disastrOS_semaphores/disastrOS_sempost.c
@@ -8,7 +8,9 @@
 
 void internal_semPost(){
   int sem_fd = running->syscall_args[0];
+
   SemDescriptor* sem_ds=SemDescriptorList_byFd(&running->sem_descriptors,sem_fd);
+  // error control for the semaphore
   if(sem_ds==0){
     printf("internal_semPost error, semaphore:%d\n",sem_fd);
     running->syscall_retvalue = DSOS_ESEMPOST;
@@ -16,18 +18,24 @@ void internal_semPost(){
   }
 
   Semaphore* sem= sem_ds->semaphore;
-  if(sem->count<=-1){
 
-  	List_insert(&ready_list, ready_list.last, (ListItem*)running);
+  if(sem->count<=-1){//if the count is below -1 i have to take and start the next process
+  	//this process is added to the tail of the ready list
+    List_insert(&ready_list, ready_list.last, (ListItem*)running);
+    //first descriptor of the semaphore in the waiting list is removed from the list and it is stored
     SemDescriptorPtr* sem_ptr =(SemDescriptorPtr*) List_detach(&sem->waiting_descriptors, (ListItem*)sem->waiting_descriptors.first);
+    //the descriptor is inserted in the descriptor list of semaphore
     List_insert(&sem->descriptors, sem->descriptors.last, (ListItem*)sem_ptr);
+    //the process in removed from the waiting list
     List_detach(&waiting_list, (ListItem*)sem_ptr->descriptor->pcb);
-
+    //the status of the running process is canged to ready
     running->status = Ready;
+    //the next running process is the process removed from the top of waiting list
     running = sem_ptr->descriptor->pcb;
   }
-  
+  //refresh the counter of the semaphore
   sem->count++;
-  running->syscall_retvalue = 0;//return value of the systemcall
+  //return value of the systemcall
+  running->syscall_retvalue = 0;
   return;
 }
--- disastrOS_semaphores/disastrOS_semwait.c
+++ disastrOS_semaphores/disastrOS_semwait.c
@@ -10,7 +10,7 @@ void internal_semWait(){
   int sem_fd = running->syscall_args[0];
   SemDescriptor* sem_ds=SemDescriptorList_byFd(&running->sem_descriptors,sem_fd);
   //error control for semaphore
-  if(sem_ds==0){
+  if(sem_ds==0){//
     printf("internal_semWait error, semaphore:%d\n",sem_fd);
     running->syscall_retvalue = DSOS_ESEMWAIT;
     return;
@@ -21,21 +21,23 @@ void internal_semWait(){
   SemDescriptorPtr* sem_ptr= sem_ds->ptr;
 
   if(sem->count<0){
-
+    //process's descriptor is removed from descriptor list
     List_detach(&sem->descriptors, (ListItem*)sem_ptr);
-
+    //process's descriptor is added in waiting descriptor list
   	List_insert(&sem->waiting_descriptors, sem->waiting_descriptors.last, (ListItem*)sem_ds->ptr);
-  	
+  	//status of process is changed in waiting
   	running->status = Waiting;
-
+    //process is added in the waiting processes list
   	List_insert(&waiting_list, waiting_list.last, (ListItem*) running);
-
+    //first element of ready list is taken
   	PCB* pcb = (PCB*) List_detach(&ready_list, (ListItem*)ready_list.first);
-
+    //the new running process is the last process taken
   	running = (PCB*)pcb;
 
   }
+  //refresh the counter of the semaphore
   sem->count--;
-  running->syscall_retvalue = 0;//return value of the systemcall
+  //return value of the systemcall
+  running->syscall_retvalue = 0;
   return;
 }
