f722b350e5088a25d278a2441a83f8d6eecf3039
berioman95 <marras.1639390@studenti.uniroma1.it>
2019-10-09 12:34:02 +0200

    Comments in semopen improved, comments in semclose added.

--- disastrOS_semaphores/disastrOS_semclose.c
+++ disastrOS_semaphores/disastrOS_semclose.c
@@ -8,29 +8,46 @@
 
 void internal_semClose(){
   int fd = running->syscall_args[0];
+
+  //Fetches the descriptor of semaphore.
   SemDescriptor* d = SemDescriptorList_byFd(&running->sem_descriptors, fd);
+  
+  //Checks if descriptor actually exists.
   if(d == 0){
    printf("ERROR - unable to find semdescriptor of the semaphore with fd = %d.\n", fd);
    running->syscall_retvalue = DSOS_ESEMCLOSE;
    return;
   }
+  
+  //Gets the semaphore.
   Semaphore* s = d->semaphore;
   if(s == NULL){
    printf("ERROR - unable to find semaphore with fd = %d from its descriptor.\n", fd);
    running->syscall_retvalue = DSOS_ESEMCLOSE;
    return;
   }
+
+  //Gets the pointer to descriptor of semaphore.
   SemDescriptorPtr* dptr = d->ptr;
   if(dptr == NULL){
    printf("ERROR - unable to find pointer to descriptor of semaphore with fd = %d from its descriptor.\n", fd);
    running->syscall_retvalue = DSOS_ESEMCLOSE;
    return;
   }
-  List_detach(&running->sem_descriptors, (ListItem *) d);
-  List_detach(&s->descriptors, (ListItem *) dptr);
 
+  //Unlinks the descriptor from the list of descriptors in the running pcb.
+  List_detach(&running->sem_descriptors, (ListItem *) d);
+
+  //Unlinks the pointer to descriptor from the list of pointers in the semaphore.
+  List_detach(&s->descriptors, (ListItem *) dptr);
+  
+  //Releases the memory where descriptor is located.
   SemDescriptor_free(d);
+
+  //Releases the memory where pointer to descriptor is located.
   SemDescriptorPtr_free(dptr);
+
+  //Checks if the process is the last one linked to semaphore, releases the semaphore memory if so.
   if(s->descriptors.size == 0 && s->waiting_descriptors.size==0){
    List_detach(&semaphores_list, (ListItem*)s);
    Semaphore_free(s);
--- disastrOS_semaphores/disastrOS_semopen.c
+++ disastrOS_semaphores/disastrOS_semopen.c
@@ -9,7 +9,7 @@
 void internal_semOpen(){
   int id = running->syscall_args[0];
 
-  //Checks if semaphore already exists
+  //Checks if semaphore already exists.
   if(SemaphoreList_byId(&semaphores_list, id) != 0){
    printf("ERROR - Semaphore with id = %d already exists.", id);
    running->syscall_retvalue = DSOS_ESEMOPEN;
@@ -18,54 +18,54 @@ void internal_semOpen(){
   
   int semaphoreCount = running->syscall_args[1];
 
-  //Checks if counter is less than 0
+  //Checks if counter is less than 0.
   if(semaphoreCount < 0){
    printf("ERROR - Unable to create semaphore with id = %d: counter is less than 0.\n", id);
    running->syscall_retvalue = DSOS_ESEMOPEN;
    return; 
   }
 
-  //Allocates the semaphore
+  //Allocates the semaphore.
   Semaphore* s = Semaphore_alloc(id, semaphoreCount);
 
-  //Checks if semaphore has been allocated
+  //Checks if semaphore has been allocated.
   if(s == NULL){
    printf("ERROR - Unable to allocate semaphore with id = %d: unknown error.\n", id);
    running->syscall_retvalue = DSOS_ESEMOPEN;
    return;
   }
 
-  //Inserts the semaphore in the list of semaphores
+  //Inserts the semaphore in the list of semaphores.
   List_insert(&semaphores_list, semaphores_list.last, (ListItem*) s);
 
-  //Allocates descriptor of semaphore
+  //Allocates descriptor of semaphore.
   SemDescriptor* d = SemDescriptor_alloc(running->last_sem_fd, s, running);
   
-  //Checks if descriptor has been allocated
+  //Checks if descriptor has been allocated.
   if(d == NULL){
    printf("ERROR - Unable to allocate descriptor of semaphore with id = %d: unknown error.\n", id);
    running->syscall_retvalue = DSOS_ESEMOPEN;
    return;
   }
-  //Allocates pointer to descriptor of semaphore
+  //Allocates pointer to descriptor of semaphore.
   SemDescriptorPtr* sem_descriptorPtr = SemDescriptorPtr_alloc(d);
 
-  //Checks if pointer to descriptor has been allocated
+  //Checks if pointer to descriptor has been allocated.
   if(sem_descriptorPtr == NULL){
    printf("ERROR - Unable to allocate pointer to descriptor of semaphore with id = %d: unknown error.\n", id);
    running->syscall_retvalue = DSOS_ESEMOPEN;
    return;
   }
-  //Insterts descriptor in the list inside the running pcb
+  //Insterts descriptor in the list inside the running pcb.
   List_insert(&running->sem_descriptors, running->sem_descriptors.last, (ListItem *) d);
 
-  //Increases the int of last semaphore inside the running pcb
+  //Increases the int of last semaphore inside the running pcb.
   running->last_sem_fd++;
   
-  //Assings the pointer to the descriptor
+  //Assings the pointer to the descriptor.
   d->ptr = sem_descriptorPtr;
 
-  //Inserts the descriptor in the list inside the semaphore
+  //Inserts the descriptor in the list inside the semaphore.
   List_insert(&s->descriptors, s->descriptors.last, (ListItem *) sem_descriptorPtr);
 
   printf("SUCCESS - Opened semaphore with id = %d.\n", id);
